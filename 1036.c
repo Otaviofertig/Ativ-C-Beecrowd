cb9b2b5c78319cecbc592f75b046a269422af924
Otaviofertig <otavio.lesss@gmail.com>
2026-03-09 10:56:34 -0300

    commit

---
+++ 1036.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double A, B, C, delta, r1, r2;
+    
+    scanf("%lf %lf %lf", &A, &B, &C);
+    
+    delta = (B * B) - (4 * A * C);
+    
+    if (A == 0 || delta < 0) {
+        printf("Impossivel calcular\n");
+    } else {
+        r1 = (-B + sqrt(delta)) / (2 * A);
+        r2 = (-B - sqrt(delta)) / (2 * A);
+        printf("R1 = %.5lf\n", r1);
+        printf("R2 = %.5lf\n", r2);
+    }
+    
+    return 0;
+}
